47aa44cacdf6d5374b499c7bf37a2464bb035559
Alex Simisi <smsxander@gmail.com>
2019-08-15 16:06:28 +0200

    feature: développement front

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @param PropertyRepository $repo
+     * @return Response
+     */
+    public function index(PropertyRepository $repo): Response
+    {
+        $properties = $repo->findLatest();
+        return $this->render('pages/home.html.twig', [
+            'properties' => $properties
+        ]);
+    }
+}
